c35ffe2bc0eb8ae1efcaa96cc655166dc8c77075
Andrey Lapechenkov <123.com>
2015-09-16 15:42:35 +0300

    cikl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-int main (int argc, char *argv[])
+using namespace std;
+int main(int argc, char *argv[])
+
 {
-    std::cout << "hello" << std::endl;
-    return 0;
+  int mas[10];
+    for (int i = 0; i < 10; i++)
+         cout << mas[i] << endl;
+
+
+
+     return 0;
+
+
 }
