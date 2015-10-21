991b6721b7abbb3ec9dfc8ea5cc8cd4184db0b4a
Andrey Lapechenkov <123.com>
2015-10-21 15:36:28 +0300

    w\newton

--- main.cpp
+++ main.cpp
@@ -1,15 +1,59 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+const double alpha = 0.1;
+
+
+double f(double x)
+
+{
+    const double alpha=0.1;
+    return sin(x)-alpha*x;
+}
+double df(double x)
+{
+    return cos(x)- alpha;
+}
+double dichotomy (
+        double x1,
+        double x2,
+        double tolerance)
+        //,TODO: pass f(x)
+
+{
+
+    while (fabs(x1-x2) > tolerance)
+    {
+        double x3=(x2+x1)/2;
+        if (f(x2)*f(x3)<0)
+            x1=x3;
+        else
+            x2=x3;
+    }
+
+
+    return x1;
+}
+double newton(double x0, double tolerance)
+{
+    double x=x0;
+    double dx;
+
+    do
+
+    {  dx=(-f(x)/df(x));
+        x+=dx;
+}
+    while( fabs(dx)> tolerance);
+    return x;
+}
+
 int main(int argc, char *argv[])
 
 {
-  int mas[10];
-    for (int i = 0; i < 10; i++)
-         cout << mas[i] << endl;
-
-
-
-     return 0;
-
-
+    double pi=2*asin(1);
+   // cout << dichotomy(pi/2, 3*pi/2, 0.001) << endl;
+    cout << newton(pi, 1e-7);
+    return 0;
 }
